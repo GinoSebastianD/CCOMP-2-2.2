4002ded0480e021367a785f376b7bf2a67e4ed79
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2023-09-05 04:54:53 -0400

    Add files via upload

---
+++ Tarea del libro 2.20-2.32/Problema 2.21 del libro de c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+    cout << "  CCC        +       +" << std::endl;
+    cout << " C           +       +" << std::endl;
+    cout << "C          +++++   +++++" << std::endl;
+    cout << " C           +       +" << std::endl;
+    cout << "  CCC        +       +" << std::endl;
+
+    return 0;
+}
+
---
+++ Tarea del libro 2.20-2.32/Problema 2.22 del lirbo de c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "*****\n****\n***\n **\n*\n " << endl;
+	
+	
+/*lo que me imprime es
+*****
+****
+***
+ **
+*          */
+}
