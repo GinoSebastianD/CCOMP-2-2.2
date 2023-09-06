1c9e64214cc655849f86a4f907420a57f315239e
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2023-09-06 04:04:16 -0500

    Update Problema 2.31 del libro de c++.cpp

--- Tarea del libro 2.20-2.32/Problema 2.31 del libro de c++.cpp
+++ Tarea del libro 2.20-2.32/Problema 2.31 del libro de c++.cpp
@@ -1,43 +1,43 @@
 
 #include <iostream>
-
+using namespace std;
 int main(){
-    using namespace std;
-	    float millas_conducidas{0},costo_galón{0}, millas_por_galón{0};
-	    float estacionamiento{0}, peaje_día{0};
-	    float costo_peaje{13.5};
+    
+    float millas_conducidas{0},costo_galon{0}, millas_por_galon{0};
+    float estacionamiento{0}, peaje_dia{0};
+    float costo_peaje{13.5};
     
     
-    cout<<"Ingrese el total de millas que conduce por día: ";
+    cout<<"Ingrese el total de millas que conduce por dia: ";
     cin>> millas_conducidas;
     
     
-    cout<<"Costo por galón de gasolina:";
-    cin>> costo_galón;
+    cout<<"Costo por galon de gasolina:";
+    cin>> costo_galon;
     
     
-    cout<<"Millas promedio por galón:";
-    cin>> millas_por_galón;
+    cout<<"Millas promedio por galon:";
+    cin>> millas_por_galon;
     
     
-    cout<<"Tarifas de estacionamiento por día:";
+    cout<<"Tarifas de estacionamiento por dia:";
     cin>> estacionamiento;
     
     
-    cout<<"Peajes por día: ";
-    cin>> peaje_día;
+    cout<<"Peajes por dia: ";
+    cin>> peaje_dia;
 
 
 
-    float galon_gasolina = millas_conducidas/millas_por_galón;
-    float costo_gasolina = costo_galón*galon_gasolina;
-    float peajes = peaje_día*costo_peaje;
+    float galon_gasolina = millas_conducidas/millas_por_galon;
+    float costo_gasolina = costo_galon*galon_gasolina;
+    float peajes = peaje_dia*costo_peaje;
 
     float costo_conduccion = costo_gasolina + peajes + estacionamiento;
     
     
 
-    cout<< "Su costo por día de conducción al trabajo es "<<costo_conduccion;
+    cout<< "Su costo por dia de conduccion al trabajo es "<<costo_conduccion;
 
     return 0;
 }
