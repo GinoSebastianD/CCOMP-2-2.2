289f895b25a5607a2c000cf9900aec616ec34a89
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2023-09-20 15:23:12 -0500

    Update euler6.cpp

--- Proyect euler/euler6.cpp
+++ Proyect euler/euler6.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-using namespace std;
-int main() {
-  int x=0;
-  int y=0;
-  long long resta;
-  for(int i=1 ; i<=100;i++){
-     x= x+(i*i);
-     y= y+i;
-  }
-  resta= (y*y)-x;
-  cout<< resta;
-  
 
-
+int main(){
+    int square_sums{0};
+    int sum_squares{0};
+    for (int i{1}; i<=100 ; i++){
+        sum_squares+=i*i;
+        square_sums+=i;
+    }
+    square_sums*=square_sums;
+    int result{square_sums - sum_squares};
+    
+    std::cout<<result;
 }
