e093cf3b0dfe88448c4ef9ba691d9f57c8342e82
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2023-09-20 15:28:16 -0500

    Update euler7.cpp

--- Proyect euler/euler7.cpp
+++ Proyect euler/euler7.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
-using namespace std;
 
-int main()
-{
-    long long num = 99;
-
-    // Initialize with 2, the first prime number
-    
-    for (int x=2 ; x<num ; x++){
-      if (num%x==0){
-        num = num/x;
-        
-      }
-      
+long long Primo(long long num){
+    long long int sum{0};
+    for (long long counter{2};counter<num; ++counter){
+        if (num%counter==0){
+            return 0;}
     }
-
-    cout << "El mayor numero primo de: " << num;
-    return 0;
+    return num;
 }
-
+long long num_primo(long long n){
+    long long x{2};
+    long long y{0};
+    while (x>0){
+        if (Primo(x)!=0){
+            y+=1;
+            if (y==n){
+                break;
+            }
+        }
+    x+=1;
+    }
+    return x;
+}
+int main(){
+    std::cout<<num_primo(10001);
+}
