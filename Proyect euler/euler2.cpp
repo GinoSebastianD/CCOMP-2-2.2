3b2d8a66ae055a0ec7f0b2d935eb7fe388ee903a
GinoSebastianD <118413024+GinoSebastianD@users.noreply.github.com>
2023-09-20 14:35:34 -0500

    Update euler2.cpp

--- Proyect euler/euler2.cpp
+++ Proyect euler/euler2.cpp
@@ -1,26 +1,33 @@
+/*Cada nuevo término en la sucesión de Fibonacci se genera sumando los dos términos anteriores. 
+Al comenzar con 1 y 2, los primeros 10 términos serán:
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+Al considerar los términos en la sucesión de Fibonacci cuyos valores no superan los cuatro millones, 
+encuentre la suma de los términos de valor par.
+*/
+
+
+
 #include <iostream>
-using namespace std;
 
-long long sumafib(int a)
-{
-  int n=1;
-  int m=2;
-  int suma=0;
+    int fibonacci (int n) {
+        if (n==0 || n==1){
+            return 1;
+        } else {
+            return fibonacci(n-2)+fibonacci(n-1);
+        }
+    }
 
-  while (n<a){
-    if ((n%2)==0)
-  {
-    suma=suma+n;
-  }
-  int x=n+m;
-  n=m;
-  m=x;
-    
-  }
-  return suma;
-}
-
-
-int main() {
-  cout<< "la suma es: "<< sumafib(4000000);
-}
+    int main (){
+        int num = 4000000;
+        int counter = 1;
+        int sum = 0;
+        while (fibonacci(counter)<num){
+            if (fibonacci(counter)%2==0){
+                sum+=fibonacci(counter);
+            }
+            ++counter;
+        }
+        
+        std::cout << sum;
+        return 0;
+    }
